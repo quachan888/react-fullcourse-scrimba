33d20d0e94a4d6f78e08ce0c16da1c0107497413
Andy Quach <quachan888@gmail.com>
2022-01-15 20:32:58 -0500

    Reinstall with create-react-app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
+import logo from "./logo.svg";
 
 function MainPage() {
     return (
-        <div className="container bg-dark text-white p-3">
+        <div>
             <Header />
             <MainContent />
             <Footer />
@@ -13,9 +15,14 @@ function MainPage() {
 
 function Header() {
     return (
-        <header>
-            <nav>
-                <img width="40px" src="https://iotvnaw69daj.i.optimole.com/AXVzL2w.n2y9~6666f/w:142/h:133/q:90/dpr:1.3/https://www.codeinwp.com/wp-content/uploads/2019/01/react-logo.png" alt="" />
+        <header className="mb-3">
+            <nav className="nav">
+                <img className="nav-logo" src={logo} />
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
             </nav>
         </header>
     );
@@ -27,8 +34,8 @@ function Footer() {
 
 function MainContent() {
     return (
-        <div>
-            <h1 className="text-info">Reason I want to learn React</h1>
+        <div className="main-content">
+            <h1>Reason I want to learn React</h1>
             <ol>
                 <li>Find a new job in Software field</li>
                 <li>Learn MERN stack</li>
